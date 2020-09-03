a1d0ce6505b3013b3d61720920c30561e3da79b1
NICE2020-2 <524359766@qq.com>
2020-09-03 15:29:14 +0800

    材料准备

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  },
+  devServer: {
+    host: '192.168.1.104',
+    port: '8080',
+    open: true
+  }
+}   
